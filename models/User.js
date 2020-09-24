cebebc6f6ee301519ae1d8212cf62f660ff69c85
Maarja <maarja.tapsi@gmail.com>
2020-09-24 16:53:25 +0300

    Documentation register user

---
+++ models/User.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const uniqueValidator = require('mongoose-unique-validator');
+const randtoken = require('rand-token');
+
+const UserSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Name required"]
+  },
+  username: {
+    type: String,
+    required: [true, "Username required"],
+    unique: [true],
+    dropDups: [true]
+  },
+  password: {
+    type: String,
+    required: [true, "Password Required"]
+  },
+  token: {
+    type: String,
+    default: () => randtoken.generate(32)
+  }
+});
+
+// Apply the uniqueValidator plugin to userSchema.
+UserSchema.plugin(uniqueValidator, { message: 'Username already exists!' });
+
+module.exports = mongoose.model("Users", UserSchema);
---
+++ routes/users.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/User');
+const Account = require('../models/Account');
+const { check,validationResult } = require('express-validator');
+const e = require('express');
+
+
+//gets back all the users
+router.get('/', async (req, res) => {
+    try{
+        const users = await User.find();
+        res.json(users);
+    } catch(err) {
+        res.json({message:err})
+    }
+});
+
+//registers an user
+router.post('/', async (req, res) => {
+    const user = new User({
+        name: req.body.name,
+        username: req.body.username,
+        password: req.body.password
+    });
+    try {
+        const savedUser = await user.save();
+        res.json(savedUser);
+    } catch (e) {
+        res.statusCode = 400
+        //res.json({message: err });
+        res.json({error: e.message});
+    }
+});
+
+//get specific user
+router.get('/:userId', async (req, res) => {
+    try{
+        const user = await User.findById(req.params.userId);
+        res.json(user);
+    } catch (err) {
+        res.json({ message: err });
+    }
+    
+});
+
+
+module.exports = router;
